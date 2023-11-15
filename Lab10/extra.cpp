2d047b599e339f60605ed235ee011e5416a6ebd5
Pedro Navarrete <navhpedro@gmail.com>
2023-11-15 10:53:55 -0600

    Add sample run to extra exercise

--- Lab10/extra.cpp
+++ Lab10/extra.cpp
@@ -1,6 +1,10 @@
 // Optional extra credit exercise (5pts)
 
 /* 
-  Write a program that determines how many consonants are in a c-string (string of characters) entered by the user.
+  Write a program that determines how many unique consonants are in a c-string (string of characters) entered by the user.
   Output the entered string and the number of consonants in the string.
+
+  Sample Run:
+    Enter a string: Hello World
+    Hello World contains 7 unique consonants.
 */  
