cb302fae8ea7000a7883866cc9dd21d3ed27000d
Pedro Navarrete <navhpedro@gmail.com>
2023-11-15 10:52:26 -0600

    Add more hints

--- Lab10/compare.cpp
+++ Lab10/compare.cpp
@@ -5,7 +5,12 @@
   Write a program that reads two names (name1, name2)
   And then prints them in alphabetical order.
   If the names are the same, print a message indicating that the names are the same.
-
+  
+  Hint: use the strcmp() function to compare the names.
+        strcmp() returns 0 if the strings are the same.
+        strcmp() returns a negative number if the first string is less than the second string.
+        strcmp() returns a positive number if the first string is greater than the second string.
+  
   Example:
     Enter name 1: John
     Enter name 2: John
--- Lab10/palindrome.cpp
+++ Lab10/palindrome.cpp
@@ -7,4 +7,10 @@
 
   Write a program that uses a function to determine if a string is a palindrome.
   A message should be displayed indicating whether or not the string is a palindrome.
+
+  Hint: use the strlen() function to determine the length of the string.
+        use a loop to compare the first character to the last character, the second character to the second to last character, etc.
+        if the characters are the same, continue the loop.
+        if the characters are different, the string is not a palindrome.
+
 */
