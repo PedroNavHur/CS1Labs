0a4279179e95e598c42911bfdde9385517f2f668
Pedro Navarrete <navhpedro@gmail.com>
2023-11-15 10:54:29 -0600

    Fix instruction

--- Lab10/extra.cpp
+++ Lab10/extra.cpp
@@ -2,7 +2,7 @@
 
 /* 
   Write a program that determines how many unique consonants are in a c-string (string of characters) entered by the user.
-  Output the entered string and the number of consonants in the string.
+  Output the entered string and the number of unique consonants in the string.
 
   Sample Run:
     Enter a string: Hello World
