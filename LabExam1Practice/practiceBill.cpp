9d6668c310ef68541597a389f15318f590c272b5
Pedro Navarrete <navhpedro@gmail.com>
2024-02-12 10:26:19 -0600

    Add Practice Lab Exam

---
+++ LabExam1Practice/practiceBill.cpp
@@ -0,0 +1,51 @@
+/* Exercises
+
+  YOUR NAME HERE: 
+
+  1. Write a program that prompts the user for their quarterly water bill for the last 4 quarters.
+     The program should then calculate and display the average monthly water bill.
+
+     If the average monthly water bill is greater than $75, the program should display a message indicating that the water bill is too high.
+     If the average monthly water bill is less than $25, the program should display a message praising the user for their low water bill.
+      Otherwise, the program should display a message indicating that the water bill is reasonable.
+
+  Example Runs:
+
+      Please input your water bill for the first quarter
+      > 100
+      Please input your water bill for the second quarter
+      > 75
+      Please input your water bill for the third quarter
+      > 50
+      Please input your water bill for the fourth quarter
+      > 125
+      The average monthly water bill is $87.50
+      The water bill is too high!
+
+      Please input your water bill for the first quarter
+      > 20
+      Please input your water bill for the second quarter
+      > 25
+      Please input your water bill for the third quarter
+      > 15
+      Please input your water bill for the fourth quarter
+      > 30
+      The average monthly water bill is $22.50
+      Good job on conserving water!
+
+      Please input your water bill for the first quarter
+      > 50
+      Please input your water bill for the second quarter
+      > 60
+      Please input your water bill for the third quarter
+      > 40
+      Please input your water bill for the fourth quarter
+      > 55
+      The average monthly water bill is $51.25
+      The water bill is reasonable.
+
+     Hints: average monthly water bill = (quarter 1 + quarter 2 + quarter 3 + quarter 4) / 4
+            water bill is too high if average monthly water bill > 75
+            water bill is too low if average monthly water bill < 25
+            water bill is reasonable if average monthly water bill is between 25 and 75
+*/
---
+++ LabExam1Practice/practiceShop.cpp
@@ -0,0 +1,36 @@
+/* Exercises
+
+  YOUR NAME HERE: 
+
+  1. The local t-shirt shop sells shirts for $12.00 each.
+     The shop offers quantity discounts given as follows:
+        10-19 shirts: 20% discount
+        20-49 shirts: 30% discount
+        50-99 shirts: 40% discount
+        100 or more shirts: 50% discount
+     The tax rate is 6.5%.
+
+      Write a program that prompts the user for the number of shirts they want to buy.
+      The program should then calculate and display the total cost of the shirts and the total cost of the shirts including tax.
+
+  Example Runs:
+     How many shirts would you like to buy?
+      > 15
+      The total cost of the shirts is $144.00
+      The total cost of the shirts including tax is $153.36
+
+      How many shirts would you like to buy?
+      > 25
+      The total cost of the shirts is $210.00
+      The total cost of the shirts including tax is $223.65
+
+      How many shirts would you like to buy?
+      > 75
+      The total cost of the shirts is $540.00
+      The total cost of the shirts including tax is $575.10
+
+      How many shirts would you like to buy?
+      > 125
+      The total cost of the shirts is $750.00
+      The total cost of the shirts including tax is $798.75
+  */
